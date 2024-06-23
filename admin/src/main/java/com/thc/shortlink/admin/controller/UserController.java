b8861f5e1ea79fc944a43ef04f38737e8a669c00
tanghuacheng <tanghc@asiainfo.com>
2024-06-23 23:31:47 +0800

    second commit

---
+++ admin/src/main/java/com/thc/shortlink/admin/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.thc.shortlink.admin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/shortlink/v1/user/{username}")
+    public String getUserByName(@PathVariable("username") String userName) {
+        return "Hi," + userName;
+    }
+
+}
