3ac1b82f7331f941ea7c2bad9cbdddb61b4f486e
tanghuacheng <tanghc@asiainfo.com>
2024-06-25 22:07:59 +0800

    feature: 提取实体类中的公共字段

--- admin/src/main/java/com/thc/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/thc/shortlink/admin/dao/entity/UserDO.java
@@ -3,6 +3,7 @@ package com.thc.shortlink.admin.dao.entity;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.thc.shortlink.admin.common.database.BaseDO;
 import lombok.Data;
 
 import java.util.Date;
@@ -12,7 +13,7 @@ import java.util.Date;
  */
 @Data
 @TableName("t_user")
-public class UserDO {
+public class UserDO extends BaseDO {
     /**
      * id
      */
@@ -48,21 +49,4 @@ public class UserDO {
      */
     private Long deletionTime;
 
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-
-    /**
-     * 更新时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Integer delFlag;
 }
